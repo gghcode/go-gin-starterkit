57f6f99f1eb021fbd28a774c3012afd1047aeecf
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-04-20 14:26:14 +0000

    Fix broken build

--- config/builder.go
+++ config/builder.go
@@ -18,14 +18,19 @@ func NewBuilder() *Builder {
 func (builder *Builder) AddConfigFile(filepath string, optional bool) *Builder {
 	builder.pipelines = append(builder.pipelines, func(v *viper.Viper) error {
 
+		return nil
 	})
+
+	return builder
 }
 
 // BindEnvs bind environment variables.
 func (builder *Builder) BindEnvs(prefix string) *Builder {
 	builder.pipelines = append(builder.pipelines, func(v *viper.Viper) error {
-
+		return nil
 	})
+
+	return builder
 }
 
 // Build return new configuration instance.
@@ -33,14 +38,14 @@ func (builder *Builder) Build() (Configuration, error) {
 	viperInstance := viper.New()
 
 	for _, pipeline := range builder.pipelines {
-		if err != pipeline(viperInstance); err != nil {
+		if err := pipeline(viperInstance); err != nil {
 			return Configuration{}, err
 		}
 	}
 
 	var result Configuration
 
-	if err != viperInstance.Unmarshal(&result); err != nil {
+	if err := viperInstance.Unmarshal(&result); err != nil {
 		return Configuration{}, err
 	}
 
