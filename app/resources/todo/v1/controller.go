ae9bcfd3d53d41ac83c8e30dea1fe26e99962297
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-04-26 13:51:19 +0000

    Add postHandler to create todo

--- app/resources/todo/v1/controller.go
+++ app/resources/todo/v1/controller.go
@@ -13,6 +13,16 @@ func NewController() *Controller {
 	return &Controller{}
 }
 
+func (c *Controller) postHandler(ctx *gin.Context) {
+	var todo Todo
+
+	if err := ctx.BindJSON(&todo); err != nil {
+		ctx.AbortWithStatus(400)
+	}
+
+	ctx.JSON(201, todo)
+}
+
 func (c *Controller) getHandler(ctx *gin.Context) {
 
 }
