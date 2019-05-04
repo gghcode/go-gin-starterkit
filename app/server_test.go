044ec6b1fe8953a2857adf4908158071b999a8ca
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-04 05:57:28 +0000

    Add unit test of registerResource function

--- app/server_test.go
+++ app/server_test.go
@@ -1,20 +1,40 @@
 package app
 
 import (
+	"github.com/gin-gonic/gin"
+	ht "github.com/gyuhwankim/go-gin-starterkit/app/http"
 	"github.com/gyuhwankim/go-gin-starterkit/config"
 	"github.com/stretchr/testify/assert"
-	// "io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestPingPong(t *testing.T) {
-	router := New(config.Configuration{}).core
+	router := setupRouter()
 	recorder := performRequest(router, "GET", "/ping")
 
 	assert.Equal(t, http.StatusOK, recorder.Code)
 	assert.Equal(t, "pong", recorder.Body.String())
+}
+
+func TestRegisterResource(t *testing.T) {
+	router := setupRouter()
+
+	registerResource(router, "VALID/PATH", []ht.Route{
+		ht.Route{
+			Method: "GET",
+			Path:   "1",
+			Handler: func(ctx *gin.Context) {
+				ctx.String(200, "VALID")
+			},
+		},
+	})
+
+	recorder := performRequest(router, "GET", "/VALID/PATH/1")
+
+	assert.Equal(t, http.StatusOK, recorder.Code)
+	assert.Equal(t, "VALID", recorder.Body.String())
 }
 
 func performRequest(router http.Handler, method, path string) *httptest.ResponseRecorder {
@@ -27,4 +47,8 @@ func performRequest(router http.Handler, method, path string) *httptest.Response
 	router.ServeHTTP(recorder, req)
 
 	return recorder
+}
+
+func setupRouter() *gin.Engine {
+	return New(config.Configuration{}).core
 }
