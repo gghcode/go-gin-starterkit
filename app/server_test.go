e3194bc7260a0ce682803853e1851b42be95b334
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-05-29 03:40:13 +0000

    Clear legacy codes

--- app/server_test.go
+++ app/server_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
-	ht "github.com/gyuhwankim/go-gin-starterkit/app/http"
 	"github.com/gyuhwankim/go-gin-starterkit/config"
 	"github.com/stretchr/testify/assert"
 )
@@ -16,25 +15,6 @@ func TestHealthyHandler(t *testing.T) {
 	recorder := performRequest(router, "GET", "/healthy")
 
 	assert.Equal(t, http.StatusOK, recorder.Code)
-}
-
-func TestRegisterResource(t *testing.T) {
-	router := setupRouter()
-
-	registerResource(router, "VALID/PATH", []ht.Route{
-		ht.Route{
-			Method: "GET",
-			Path:   "1",
-			Handler: func(ctx *gin.Context) {
-				ctx.String(200, "VALID")
-			},
-		},
-	})
-
-	recorder := performRequest(router, "GET", "/VALID/PATH/1")
-
-	assert.Equal(t, http.StatusOK, recorder.Code)
-	assert.Equal(t, "VALID", recorder.Body.String())
 }
 
 func performRequest(router http.Handler, method, path string) *httptest.ResponseRecorder {
