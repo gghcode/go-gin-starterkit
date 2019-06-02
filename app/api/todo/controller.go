9065444f32caeeb98ea9e02d8fe71f642a0f5099
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-06-02 08:27:08 +0000

    Implement prototype of todo controller

--- app/api/todo/controller.go
+++ app/api/todo/controller.go
@@ -1,6 +1,9 @@
 package todo
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gyuhwankim/go-gin-starterkit/app/api/common"
+)
 
 // Controller handles http request.
 type Controller struct {
@@ -16,8 +19,26 @@ func NewController(repo Repository) *Controller {
 
 // RegisterRoutes register handler routes.
 func (controller Controller) RegisterRoutes(router gin.IRouter) {
+	router.Handle("POST", "/", controller.createTodo)
 	router.Handle("GET", "/", controller.getAllTodos)
 	router.Handle("GET", "/:id", controller.getTodoByTodoID)
+	router.Handle("PUT", "/:id", controller.updateTodoByTodoID)
+	router.Handle("DELETE", "/:id", controller.removeTodoByTodoID)
+}
+
+func (controller *Controller) createTodo(ctx *gin.Context) {
+	var todo Todo
+
+	if err := ctx.BindJSON(&todo); err != nil {
+		ctx.AbortWithError(400, err)
+	}
+
+	createdTodo, err := controller.repo.createTodo(todo)
+	if err != nil {
+		ctx.AbortWithError(500, err)
+	}
+
+	ctx.JSON(201, createdTodo)
 }
 
 func (controller *Controller) getAllTodos(ctx *gin.Context) {
@@ -33,9 +54,44 @@ func (controller *Controller) getTodoByTodoID(ctx *gin.Context) {
 	todoID := ctx.Param("id")
 
 	todo, err := controller.repo.getTodoByTodoID(todoID)
-	if err != nil {
-		ctx.String(500, err.Error())
+	if err == common.ErrEntityNotFound {
+		ctx.AbortWithStatus(404)
+	} else if err != nil {
+		ctx.AbortWithStatusJSON(500, map[string]string{
+			"error": err.Error(),
+		})
 	}
 
 	ctx.JSON(200, todo)
+}
+
+func (controller *Controller) updateTodoByTodoID(ctx *gin.Context) {
+	todoID := ctx.Param("id")
+	todo := Todo{}
+
+	if err := ctx.BindJSON(&todo); err != nil {
+		ctx.AbortWithError(400, err)
+	}
+
+	todo, err := controller.repo.updateTodoByTodoID(todoID, todo)
+	if err == common.ErrEntityNotFound {
+		ctx.AbortWithError(404, err)
+	} else if err != nil {
+		ctx.AbortWithError(500, err)
+	}
+
+	ctx.JSON(200, todo)
+}
+
+func (controller *Controller) removeTodoByTodoID(ctx *gin.Context) {
+	todoID := ctx.Param("id")
+
+	removedTodoID, err := controller.repo.removeTodoByTodoID(todoID)
+	if err == common.ErrEntityNotFound {
+		ctx.AbortWithError(404, err)
+	} else if err != nil {
+		ctx.AbortWithError(500, err)
+	}
+
+	ctx.JSON(204, removedTodoID)
 }
