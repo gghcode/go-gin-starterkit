01d8e489440fc155bcffaea9215d7d796eccfd91
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-06-02 08:27:08 +0000

    Implement prototype of todo controller

--- app/api/todo/controller_test.go
+++ app/api/todo/controller_test.go
@@ -1,12 +1,16 @@
 package todo
 
 import (
+	"bytes"
 	"encoding/json"
-	"github.com/stretchr/testify/mock"
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/gyuhwankim/go-gin-starterkit/app/api/common"
+	"github.com/stretchr/testify/mock"
 
 	"github.com/gin-gonic/gin"
 	uuid "github.com/satori/go.uuid"
@@ -20,8 +24,6 @@ type controllerTestSuite struct {
 	ginEngine  *gin.Engine
 	controller *Controller
 	mockRepo   *mockRepository
-
-	requestHTTP func(router http.Handler, method, path string) *httptest.ResponseRecorder
 }
 
 type mockRepository struct {
@@ -30,26 +32,27 @@ type mockRepository struct {
 
 func (repo *mockRepository) createTodo(todo Todo) (Todo, error) {
 	args := repo.Called(todo)
-
 	return args.Get(0).(Todo), args.Error(1)
 }
 
 func (repo *mockRepository) getTodos() ([]Todo, error) {
 	args := repo.Called()
-
 	return args.Get(0).([]Todo), args.Error(1)
 }
 
 func (repo *mockRepository) getTodoByTodoID(todoID string) (Todo, error) {
-	panic("Not Implement")
+	args := repo.Called(todoID)
+	return args.Get(0).(Todo), args.Error(1)
 }
 
 func (repo *mockRepository) updateTodoByTodoID(todoID string, todo Todo) (Todo, error) {
-	panic("Not Implement")
+	args := repo.Called(todoID, todo)
+	return args.Get(0).(Todo), args.Error(1)
 }
 
 func (repo *mockRepository) removeTodoByTodoID(todoID string) (string, error) {
-	panic("Not Implement")
+	args := repo.Called(todoID)
+	return args.String(0), args.Error(1)
 }
 
 func TestControllerTestSuite(t *testing.T) {
@@ -61,21 +64,11 @@ func (suite *controllerTestSuite) SetupTest() {
 	suite.mockRepo = &mockRepository{}
 	suite.controller = NewController(suite.mockRepo)
 	suite.controller.RegisterRoutes(suite.ginEngine)
-	suite.requestHTTP = func(router http.Handler, method, path string) *httptest.ResponseRecorder {
-		req, err := http.NewRequest(method, path, nil)
-		if err != nil {
-			panic(err)
-		}
-
-		recorder := httptest.NewRecorder()
-		router.ServeHTTP(recorder, req)
-
-		return recorder
-	}
 }
 
 func (suite *controllerTestSuite) TestShouldGetTodos() {
-	returnTodos := []Todo{
+	expectedCode := http.StatusOK
+	expectedTodos := []Todo{
 		Todo{
 			ID:       uuid.NewV4(),
 			Title:    "title",
@@ -85,35 +78,196 @@ func (suite *controllerTestSuite) TestShouldGetTodos() {
 
 	suite.mockRepo.
 		On("getTodos").
-		Return(returnTodos, nil)
+		Return(expectedTodos, nil)
 
 	req, err := http.NewRequest("GET", "/", nil)
-
 	require.NoError(suite.T(), err)
 
-	recorder := httptest.NewRecorder()
-	suite.ginEngine.ServeHTTP(recorder, req)
+	actual := getActualResponse(suite, req)
 
-	expected, err := json.Marshal(returnTodos)
-	actual, err := ioutil.ReadAll(recorder.Body)
-
-	require.Equal(suite.T(), http.StatusOK, recorder.Code)
-	require.Equal(suite.T(), string(expected), string(actual))
+	require.Equal(suite.T(), expectedCode, actual.Code)
+	requireEqualJSON(suite.T(), expectedTodos, actual.Body)
 }
 
-// func TestGetAllTodos(t *testing.T) {
-// 	engine := setupTestcase()
-// 	recorder := requestHTTP(engine, "GET", "/")
+func (suite *controllerTestSuite) TestShouldGetTodoByTodoID() {
+	expectedCode := http.StatusOK
+	expectedTodoID := uuid.NewV4()
+	expectedTodo := Todo{
+		ID:       expectedTodoID,
+		Title:    "title",
+		Contents: "contents",
+	}
 
-// 	assert.Equal(t, http.StatusOK, recorder.Code)
+	suite.mockRepo.
+		On("getTodoByTodoID", expectedTodoID.String()).
+		Return(expectedTodo, nil)
 
-// 	bodyBytes, err := ioutil.ReadAll(recorder.Body)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
+	req, err := http.NewRequest("GET", "/"+expectedTodoID.String(), nil)
+	require.NoError(suite.T(), err)
 
-// 	bodyString := string(bodyBytes)
-// 	expected := "{\"fasdf\":\"fasdf\"}"
+	actual := getActualResponse(suite, req)
 
-// 	assert.Equal(t, expected, bodyString)
-// }
+	require.Equal(suite.T(), expectedCode, actual.Code)
+	requireEqualJSON(suite.T(), expectedTodo, actual.Body)
+}
+
+func (suite *controllerTestSuite) TestShouldBeNotFoundWhenGetTodoByTodoID() {
+	expectedCode := http.StatusNotFound
+	notExistsTodoID := uuid.NewV4()
+
+	suite.mockRepo.
+		On("getTodoByTodoID", notExistsTodoID.String()).
+		Return(Todo{}, common.ErrEntityNotFound)
+
+	req, err := http.NewRequest("GET", "/"+notExistsTodoID.String(), nil)
+	require.NoError(suite.T(), err)
+
+	actual := getActualResponse(suite, req)
+
+	require.Equal(suite.T(), expectedCode, actual.Code)
+}
+
+func (suite *controllerTestSuite) TestShouldBeInternalErrorWhenGetTodoByTodoID() {
+	expectedCode := http.StatusInternalServerError
+	todoID := uuid.NewV4()
+
+	suite.mockRepo.
+		On("getTodoByTodoID", todoID.String()).
+		Return(Todo{}, errors.New("Occurred error."))
+
+	req, err := http.NewRequest("GET", "/"+todoID.String(), nil)
+	require.NoError(suite.T(), err)
+
+	actual := getActualResponse(suite, req)
+
+	require.Equal(suite.T(), expectedCode, actual.Code)
+}
+
+func (suite *controllerTestSuite) TestShouldBeCreatedTodo() {
+	expectedCode := http.StatusCreated
+	expectedTodoID := uuid.NewV4()
+	expectedTodo := Todo{
+		Title:    "new title",
+		Contents: "new contents",
+	}
+
+	suite.mockRepo.
+		On("createTodo", expectedTodo).
+		Return(Todo{
+			ID:       expectedTodoID,
+			Title:    expectedTodo.Title,
+			Contents: expectedTodo.Contents,
+		}, nil)
+
+	expectedTodoJSON, err := json.Marshal(expectedTodo)
+	require.NoError(suite.T(), err)
+
+	req, err := http.NewRequest("POST", "/", bytes.NewBuffer(expectedTodoJSON))
+	require.NoError(suite.T(), err)
+
+	actual := getActualResponse(suite, req)
+	expectedTodo.ID = expectedTodoID
+
+	require.Equal(suite.T(), expectedCode, actual.Code)
+	requireEqualJSON(suite.T(), expectedTodo, actual.Body)
+}
+
+func (suite *controllerTestSuite) TestShouldBeUpdatedTodo() {
+	expectedCode := http.StatusOK
+	expectedTodo := Todo{
+		ID:       uuid.NewV4(),
+		Title:    "updated title",
+		Contents: "updated contents",
+	}
+
+	suite.mockRepo.
+		On("updateTodoByTodoID", expectedTodo.ID.String(), expectedTodo).
+		Return(expectedTodo, nil)
+
+	expectedTodoJSON, err := json.Marshal(expectedTodo)
+	require.NoError(suite.T(), err)
+
+	req, err := http.NewRequest("PUT", "/"+expectedTodo.ID.String(),
+		bytes.NewBuffer(expectedTodoJSON))
+	require.NoError(suite.T(), err)
+
+	actual := getActualResponse(suite, req)
+
+	require.Equal(suite.T(), expectedCode, actual.Code)
+	requireEqualJSON(suite.T(), expectedTodo, actual.Body)
+}
+
+func (suite *controllerTestSuite) TestShouldBeNotFoundWhenUpdateTodoByTodoID() {
+	expectedCode := http.StatusNotFound
+	notExistsTodo := Todo{
+		ID:       uuid.NewV4(),
+		Title:    "not exists title",
+		Contents: "not exists contents",
+	}
+
+	suite.mockRepo.
+		On("updateTodoByTodoID", notExistsTodo.ID.String(), notExistsTodo).
+		Return(Todo{}, common.ErrEntityNotFound)
+
+	notExistsTodoJSON, err := json.Marshal(notExistsTodo)
+	require.NoError(suite.T(), err)
+
+	req, err := http.NewRequest("PUT", "/"+notExistsTodo.ID.String(),
+		bytes.NewBuffer(notExistsTodoJSON))
+	require.NoError(suite.T(), err)
+
+	actual := getActualResponse(suite, req)
+
+	require.Equal(suite.T(), expectedCode, actual.Code)
+}
+
+func (suite *controllerTestSuite) TestShouldBeRemovedTodo() {
+	expectedCode := http.StatusNoContent
+	expectedTodoID := uuid.NewV4()
+
+	suite.mockRepo.
+		On("removeTodoByTodoID", expectedTodoID.String()).
+		Return(expectedTodoID.String(), nil)
+
+	req, err := http.NewRequest("DELETE", "/"+expectedTodoID.String(), nil)
+	require.NoError(suite.T(), err)
+
+	actual := getActualResponse(suite, req)
+
+	require.Equal(suite.T(), expectedCode, actual.Code)
+}
+
+func (suite *controllerTestSuite) TestShouldBeNotFoundWhenRemoveTodoByTodoID() {
+	expectedCode := http.StatusNotFound
+	notExistsTodoID := uuid.NewV4()
+
+	suite.mockRepo.
+		On("removeTodoByTodoID", notExistsTodoID.String()).
+		Return("", common.ErrEntityNotFound)
+
+	req, err := http.NewRequest("DELETE", "/"+notExistsTodoID.String(), nil)
+	require.NoError(suite.T(), err)
+
+	actual := getActualResponse(suite, req)
+
+	require.Equal(suite.T(), expectedCode, actual.Code)
+}
+
+func getActualResponse(suite *controllerTestSuite, req *http.Request) *httptest.ResponseRecorder {
+	recorder := httptest.NewRecorder()
+
+	suite.ginEngine.ServeHTTP(recorder, req)
+
+	return recorder
+}
+
+func requireEqualJSON(t *testing.T, data interface{}, buf *bytes.Buffer) {
+	expected, err := json.Marshal(data)
+
+	require.NoError(t, err)
+
+	actual, err := ioutil.ReadAll(buf)
+
+	require.NoError(t, err)
+	require.Equal(t, string(expected), string(actual))
+}
